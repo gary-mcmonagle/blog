6a0683827a6296ca871bb97b3cb35b221d89f6b9
Gary McMonagle <garybmcmonagle@gmail.com>
2022-11-21 22:01:23 +0000

    test with api

--- blog-fe/src/api/getWeatherForecast.ts
+++ blog-fe/src/api/getWeatherForecast.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const getWeatherForecast = async (accessToken: string) => {
     const bearer = `Bearer ${accessToken}`
-      const data = await axios.get("https://garyblogservices.azurewebsites.net/api/GaryTest", 
+      const data = await axios.get("https://blogservicesadmin-function.azurewebsites.net/api/HttpTrigger1", 
   {
     headers: {
         Authorization: bearer
