81f2e57acefd087f1b94bc6c76ad4db9fc0f525e
Gary McMonagle <garybmcmonagle@gmail.com>
2022-11-27 10:54:33 +0000

    add service functions

---
+++ BlogAdminServices/TestCosmosTrigger.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+
+namespace Blog.Pub
+{
+    public class ToDoItemLookup
+    {
+        public string ToDoItemId { get; set; }
+
+        public string ToDoItemPartitionKeyValue { get; set; }
+    }
+
+        public class ToDoItem
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("partitionKey")]
+        public string PartitionKey { get; set; }
+
+        public string Description { get; set; }
+    }
+    public static class TestCosmosTrigger
+    {
+        [FunctionName("TestCosmosTrigger")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "ToDoList",
+                collectionName: "Items",
+                ConnectionStringSetting = "CosmosDBConnection",
+                Id = "{Query.id}",
+                PartitionKey = "{Query.partitionKey}")] ToDoItem toDoItem,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (toDoItem == null)
+            {
+                log.LogInformation($"ToDo item not found");
+            }
+            else
+            {
+                log.LogInformation($"Found ToDo item, Description={toDoItem.Description}");
+            }
+            return new OkResult();
+        }
+    }
+}
